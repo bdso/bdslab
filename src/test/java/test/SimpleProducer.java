79391dd7eda2c236b3a21bff37f0102701acbf1e
Leo-Phan <leo@Chip-Minis-Mac-mini.local>
2017-11-21 11:36:09 +0700

    Done to submit topocluster

--- src/test/java/test/SimpleProducer.java
+++ src/test/java/test/SimpleProducer.java
@@ -36,11 +36,11 @@ public class SimpleProducer {
                 /**
                  * Audio
                  */
-                //                + "\"request\":\"/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJVSVpBIiwiYXVkIjoidWl6YS5pbyIsImlhdCI6MTUxMDk5Mzg1MCwiZXhwIjoxNTExNTk4NjUwLCJlbnRpdHlfaWQiOiIxIiwiZW50aXR5X3RpdGxlIjoiSGFyZGNvZGUgdGl0bGUiLCJlbnRpdHlfc3RyZWFtX3R5cGUiOiJ2b2QiLCJ1c2VyX2lkIjoiNTdhYTNhMGViNTU1NDIwYTk0NWEyN2I0N2NlOWVmMmYiLCJzdWIiOiI1N2FhM2EwZWI1NTU0MjBhOTQ1YTI3YjQ3Y2U5ZWYyZiJ9.GRyg7m8LEJB7G4fCLKTpJF8k111L5x1Ex452IleAKWw/57aa3a0eb555420a945a27b47ce9ef2f-transcode-output/7f042f55-ee4a-4bd0-b043-95ed88233546/other/audio/unk-1/mp4a/64000/64000-16.m4s\","
+                + "\"request\":\"/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJVSVpBIiwiYXVkIjoidWl6YS5pbyIsImlhdCI6MTUxMDk5Mzg1MCwiZXhwIjoxNTExNTk4NjUwLCJlbnRpdHlfaWQiOiIxIiwiZW50aXR5X3RpdGxlIjoiSGFyZGNvZGUgdGl0bGUiLCJlbnRpdHlfc3RyZWFtX3R5cGUiOiJ2b2QiLCJ1c2VyX2lkIjoiNTdhYTNhMGViNTU1NDIwYTk0NWEyN2I0N2NlOWVmMmYiLCJzdWIiOiI1N2FhM2EwZWI1NTU0MjBhOTQ1YTI3YjQ3Y2U5ZWYyZiJ9.GRyg7m8LEJB7G4fCLKTpJF8k111L5x1Ex452IleAKWw/57aa3a0eb555420a945a27b47ce9ef2f-transcode-output/7f042f55-ee4a-4bd0-b043-95ed88233546/other/audio/unk-1/mp4a/64000/64000-16.m4s\","
                 /**
                  * Video
                  */
-                + "\"request\":\"/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJVSVpBIiwiYXVkIjoidWl6YS5pbyIsImlhdCI6MTUxMDk5Mzg1MCwiZXhwIjoxNTExNTk4NjUwLCJlbnRpdHlfaWQiOiIxIiwiZW50aXR5X3RpdGxlIjoiSGFyZGNvZGUgdGl0bGUiLCJlbnRpdHlfc3RyZWFtX3R5cGUiOiJ2b2QiLCJ1c2VyX2lkIjoiNTdhYTNhMGViNTU1NDIwYTk0NWEyN2I0N2NlOWVmMmYiLCJzdWIiOiI1N2FhM2EwZWI1NTU0MjBhOTQ1YTI3YjQ3Y2U5ZWYyZiJ9.GRyg7m8LEJB7G4fCLKTpJF8k111L5x1Ex452IleAKWw/57aa3a0eb555420a945a27b47ce9ef2f-transcode-output/7f042f55-ee4a-4bd0-b043-95ed88233546/other/video/avc1/1280x720/2000000-16.m4s\","
+                //                + "\"request\":\"/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJVSVpBIiwiYXVkIjoidWl6YS5pbyIsImlhdCI6MTUxMDk5Mzg1MCwiZXhwIjoxNTExNTk4NjUwLCJlbnRpdHlfaWQiOiIxIiwiZW50aXR5X3RpdGxlIjoiSGFyZGNvZGUgdGl0bGUiLCJlbnRpdHlfc3RyZWFtX3R5cGUiOiJ2b2QiLCJ1c2VyX2lkIjoiNTdhYTNhMGViNTU1NDIwYTk0NWEyN2I0N2NlOWVmMmYiLCJzdWIiOiI1N2FhM2EwZWI1NTU0MjBhOTQ1YTI3YjQ3Y2U5ZWYyZiJ9.GRyg7m8LEJB7G4fCLKTpJF8k111L5x1Ex452IleAKWw/57aa3a0eb555420a945a27b47ce9ef2f-transcode-output/7f042f55-ee4a-4bd0-b043-95ed88233546/other/video/avc1/1280x720/2000000-16.m4s\","
                 + "\"response\":\"200\","
                 + "\"bytes\":\"1189104\","
                 + "\"referrer\":\"\\\"http://localhost:1343/detail/chu-cho-tinh-nghich\\\"\","
